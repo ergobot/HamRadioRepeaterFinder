1527f33066e764734be3b327978446e563a72943
Sean OBryan <stobryan@gmail.com>
2013-05-19 15:32:55 -0500

    First commit

---
+++ HamRadioRepeaterFinder/build/source/buildConfig/debug/org/area515/hamradiorepeaterfinder/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.area515.hamradiorepeaterfinder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
